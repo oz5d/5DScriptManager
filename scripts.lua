38fc57267ec7b0b6e596b4fe036642a2bba873d4
oz5d <101996076+oz5d@users.noreply.github.com>
2023-04-30 00:25:57 +0100

    Update scripts.lua

--- scripts.lua
+++ scripts.lua
@@ -2,7 +2,7 @@ getgenv().scripts = {
   debug = {
     name = "Debug",
     scriptRepository = "debug/",
-    versions = { "1" },
+    versions = {"1"},
     latestVersion = "1",
     fileName = "debug.lua",
   },
