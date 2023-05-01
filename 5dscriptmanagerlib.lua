571b6522e7619c39da1b93dfcbcd32b74d91f359
oz5d <101996076+oz5d@users.noreply.github.com>
2023-05-01 21:57:50 +0100

    Update 5dscriptmanagerlib.lua

--- 5dscriptmanagerlib.lua
+++ 5dscriptmanagerlib.lua
@@ -1,10 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/oz5d/5DScriptManager/main/scripts.lua"))()
 getgenv().locationPrefix = "https://raw.githubusercontent.com/oz5d/5DScriptManager/main/scripts/"
-getgenv().loadScript = function(scriptTableKey,scriptVersion)
-	local scriptInfo = scripts[scriptTableKey]
-	if scriptInfo == nil then
-		error("Script table key invalid!: '"..scriptTableKey.."'")
-	end
+getgenv().loadScript = function(scriptInfo,scriptVersion)
 	scriptVersion = scriptVersion or scriptInfo.latestVersion
 	if table.find(scriptInfo.versions,scriptVersion) == nil then
 		error("Script version invalid!: '"..scriptVersion.."'")
