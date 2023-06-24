c18555ae987f1f6347431e6ce96760415b3d96d3
oz5d <101996076+oz5d@users.noreply.github.com>
2023-06-24 15:42:47 +0100

    Update and rename scripts.lua to ScriptInfoTable.lua

---
+++ ScriptInfoTable.lua
@@ -0,0 +1,8 @@
+getgenv().scriptInfoTable = {
+  debug = {
+    name = "Debug",
+    description = "check if shit works",
+    versions = {"1"},
+    filesToLoad = "debug.lua",
+  },
+}
--- scripts.lua
+++
@@ -1,9 +0,0 @@
-getgenv().scripts = {
-  debug = {
-    name = "Debug",
-    scriptRepository = "debug",
-    versions = {"1"},
-    latestVersion = "1",
-    fileName = "debug.lua",
-  },
-}
